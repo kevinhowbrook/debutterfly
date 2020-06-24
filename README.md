dceede9147461986406da70cef35e5c9e75d4460
Anastasia Widyadari <widi.adari@hotmail.com>
2020-06-24 18:09:03 +0100

    menus updated

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # A sample website for Coders of Colour
+
+# Debutterfly CIC
+
+A python website made using Django and Wagtail
+
+
--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'home',
     'search',
     'blog',
+    'menus',
     'contact',
     'flex',
     'streams',
